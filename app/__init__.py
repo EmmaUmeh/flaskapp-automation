1ff7e77ac23278053241d5632e8287566f343553
Emmanuel Umeh <emmachinemerem04@gmail.com>
2024-01-25 08:58:42 -0800

    Added_promtheus

--- app/__init__.py
+++ app/__init__.py
@@ -1,51 +1,28 @@
-# from flask import Flask
-# from .automate import add
-
-# app = Flask(__name__)
-
-# @app.route('/addition')
-# def addition():
-#     # Call the add function
-#     sum_result = add(5, 3)
-#     return f'The sum of the Automated Deployment is: {sum_result}'
-
-# @app.route('/')
-# def default_route():
-#     return 'Automated Deployment'
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-  
 from flask import Flask
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from prometheus_client import make_wsgi_app
-
-# Import the add function from automate module
+from prometheus_flask_exporter import PrometheusMetrics
 from .automate import add
 
-# Create the Flask app
-app = Flask(__name__)
+# Install prometheus-flask-exporter
+try:
+    from prometheus_flask_exporter import PrometheusMetrics
+except ImportError:
+    print("prometheus-flask-exporter is not installed. Installing...")
+    import subprocess
+    subprocess.run(["pip", "install", "prometheus-flask-exporter"], check=True)
+    from prometheus_flask_exporter import PrometheusMetrics
 
-# Add a route for the addition function
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
 @app.route('/addition')
 def addition():
     # Call the add function
     sum_result = add(5, 3)
     return f'The sum of the Automated Deployment is: {sum_result}'
 
-# Add a default route
 @app.route('/')
 def default_route():
     return 'Automated Deployment'
 
-# Wrap the Flask app with Prometheus WSGI middleware
-app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-    '/metrics': make_wsgi_app()
-})
-
-# Run the Flask app
 if __name__ == '__main__':
-    
     app.run(debug=True)
-
